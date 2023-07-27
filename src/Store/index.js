bd8571783fb012578a97399a86ac40f935eeb81e
chicmicdev <rohitkumar@chicmic.co.in>
2023-07-27 15:48:43 +0530

    feat: intial commit

---
+++ src/Store/index.js
@@ -0,0 +1,46 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import api from '../Services/Api/api';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import common from './Common';
+import loader from './Loader';
+
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['common'],
+};
+const reducers = combineReducers({
+  common,
+  loader,
+  [api.reducerPath]: api.reducer,
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(api.middleware);
+    return middlewares;
+  },
+});
+
+const persistor = persistStore(store);
+setupListeners(store.dispatch);
+export { store, persistor };
---
+++ src/shared/utils/Constants.ts
@@ -0,0 +1,3 @@
+const STRING: string = 'dds';
+// eslint-disable-next-line import/prefer-default-export
+export { STRING };
