842e29d8355088bbc7326e7760610b7742470fa0
chicmicdev <rohitkumar@chicmic.co.in>
2023-07-27 15:48:43 +0530

    feat: intial commit

---
+++ src/Services/Api/api.ts
@@ -0,0 +1,31 @@
+import { CreateApi, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_BASE_URL } from './Constants';
+import { reactHooksModuleName } from '@reduxjs/toolkit/dist/query/react/module';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_BASE_URL,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  prepareHeaders: async (headers, { getState }) => {
+    const { token } = getState().common;
+    if (token) {
+      headers.append('authorization', `${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithInterceptor = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    // here you can deal with 401 error
+  }
+  return result;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const api: any = createApi({
+  baseQuery: baseQueryWithInterceptor,
+  endpoints: () => ({}),
+});
+
+export default api;
