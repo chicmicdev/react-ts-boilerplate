c14741b8e061d8751c9a1e2e65a0081def367646
chicmicdev <rohitkumar@chicmic.co.in>
2023-07-31 18:56:15 +0530

    fix: route and types added

---
+++ src/Routes/AuthRoutes.ts
@@ -0,0 +1,9 @@
+const AUTH_ROUTES: string[] = [
+  // {
+  //   path: '/forgot-password',
+  //   component: ForgotPassword,
+  //   title: 'Forgot Password',
+  // },
+];
+
+export default AUTH_ROUTES;
