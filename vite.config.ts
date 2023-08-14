11dff04ebdc5c64eb7f903bdbd71249597ed16b7
ƤɑⱤɑｍᴠééⱤ <Paramveer.singh@chicmic.co.in>
2023-08-14 13:05:26 +0530

    feat: handled vite config to access env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import Checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    Checker({
-      typescript: true,
-      overlay: true,
-    })],
-});
 
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react(),
+      Checker({
+        typescript: true,
+        overlay: true,
+      })],
+  });
+}
