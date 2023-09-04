e2d30b6693f6fc0f520898b929f93ac2a404d93e
Ajaypal <ajaypal.singh@chicmic.co.in>
2023-09-04 12:53:33 +0530

    feat: added api usage and rendering of data

--- src/Views/Dashboard/Dashboard.tsx
+++ src/Views/Dashboard/Dashboard.tsx
@@ -1,7 +1,49 @@
-import { useDemoApiQuery } from '../../Services/Api/module/demoApi';
+import { useEffect, useState } from 'react';
+import {
+  useDemoApiQuery,
+  usePhotosMutation,
+} from '../../Services/Api/module/demoApi';
+
+interface ArrayList {
+  albumId: number;
+  id: number;
+  thumbnailUrl: string;
+  title: string;
+  url: string;
+}
 
 export default function Dashboard() {
   const { data, error } = useDemoApiQuery('');
-  console.log(data, error);
-  return <div>Dashboard</div>;
+  const [photos] = usePhotosMutation();
+  console.log('data, error: ', data, error);
+
+  const [list, setList] = useState<ArrayList[]>([]);
+
+  const fetchPhotos = async () => {
+    try {
+      const response = (await photos('').unwrap()) as ArrayList[];
+      setList(response);
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      Dashboard
+      <div className="row">
+        {list?.slice(0, 30)?.map((photo) => (
+          <div className="col-3" key={photo.id}>
+            <span>{photo.title}</span>
+            <img className="img-fluid" src={photo.thumbnailUrl} alt="logo" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
