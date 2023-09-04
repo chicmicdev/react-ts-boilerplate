8c24d097d94550a825f209a25889338218e835cc
Ajaypal <ajaypal.singh@chicmic.co.in>
2023-09-04 13:03:27 +0530

    feat: added rendering of data in dashboard

--- src/Views/Dashboard/Dashboard.tsx
+++ src/Views/Dashboard/Dashboard.tsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from 'react';
 import {
   useDemoApiQuery,
-  usePhotosMutation,
+  // usePhotosMutation,
 } from '../../Services/Api/module/demoApi';
 
-interface ArrayList {
-  albumId: number;
-  id: number;
-  thumbnailUrl: string;
-  title: string;
-  url: string;
-}
+// interface ArrayList {
+//   albumId: number;
+//   id: number;
+//   thumbnailUrl: string;
+//   title: string;
+//   url: string;
+// }
 
 export default function Dashboard() {
   const { data, error } = useDemoApiQuery('');
-  const [photos] = usePhotosMutation();
   console.log('data, error: ', data, error);
 
-  const [list, setList] = useState<ArrayList[]>([]);
+  // const [photos] = usePhotosMutation();
+  // const [list, setList] = useState<ArrayList[]>([]);
+  // console.log('list: ', list);
 
-  const fetchPhotos = async () => {
-    try {
-      const response = (await photos('').unwrap()) as ArrayList[];
-      setList(response);
-    } catch (err) {
-      console.log('error: ', err);
-    }
-  };
+  // const fetchPhotos = async () => {
+  //   try {
+  //     const response = (await photos('').unwrap()) as ArrayList[];
+  //     setList(response);
+  //   } catch (err) {
+  //     console.log('error: ', err);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchPhotos();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   fetchPhotos();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <div>
       Dashboard
-      <div className="row">
+      {/* <div className="row">
         {list?.slice(0, 30)?.map((photo) => (
           <div className="col-3" key={photo.id}>
             <span>{photo.title}</span>
             <img className="img-fluid" src={photo.thumbnailUrl} alt="logo" />
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
